3a020afd428904f0b1da7bf25594326bdcc2c7ea
gandradep <eventsonido@gmail.com>
2022-09-05 13:46:48 +0100

    Mobile menu: activate hamburger button

--- menu.js
+++ menu.js
@@ -1,1 +1,9 @@
-console.log('test');
+let hamburgerSymbol = document.getElementById('hamburgerSymbol');
+let popUpMenuMobile = document.getElementById('popUpMenuMobile');
+function openMenu(){
+  popUpMenuMobile.classList.add('showMenu');
+  console.log('test');
+
+}
+
+hamburgerSymbol.addEventListener('click', openMenu);
