b5835130006d17c9ce49a7a3e07d39c386700813
vflorin1995 <vflorin1995@gmail.com>
2022-09-05 16:05:50 +0300

    Add close menu functionality

--- menu.js
+++ menu.js
@@ -1,9 +1,24 @@
 let hamburgerSymbol = document.getElementById('hamburgerSymbol');
 let popUpMenuMobile = document.getElementById('popUpMenuMobile');
+let closeMenuSymbol = document.getElementById('closeMenuSymbol');
+let menuListItems = Array.from(document.querySelectorAll('ul .navBarColor'));
+
+console.log(menuListItems);
+
 function openMenu(){
   popUpMenuMobile.classList.add('showMenu');
-  console.log('test');
-
 }
 
+function closeMenu(){
+  popUpMenuMobile.classList.remove('showMenu');
+}
+
+
 hamburgerSymbol.addEventListener('click', openMenu);
+closeMenuSymbol.addEventListener('click', closeMenu);
+
+
+menuListItems.forEach(element => {
+  element.addEventListener('click', closeMenu);
+  
+});
