8c209ab5d29f89c851bae4d004e73dcb7fb3e292
vflorin1995 <vflorin1995@gmail.com>
2022-09-07 17:36:42 +0300

    Add form email validation

--- formValidation.js
+++ formValidation.js
@@ -1,1 +1,15 @@
-console.log('test');
+let email = document.getElementById('emailAddress');
+let form = document.getElementById('contactForm');
+
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    let inputEmail = email.value;
+    let inputEmailLow = inputEmail.toLowerCase();
+    if(inputEmail === inputEmailLow) {
+        // form.submit();
+        console.log('Email was sent');
+    } else {
+        console.log('Please do not use caps');
+    }
+});
