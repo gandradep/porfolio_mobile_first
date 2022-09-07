1948c1294639bdd71a925ff1bfe8f7c9ae24b9c7
gandradep <eventsonido@gmail.com>
2022-09-07 09:32:34 +0100

    Modal window projects for mobile and desktop

--- worksModal.js
+++ worksModal.js
@@ -2,7 +2,7 @@ const worksInfo = {
   title:
   [
     'Keeping track of hundreds of components',
-    'Project 2', 'Project 3', 'Project 4', 'Project 5', 'Project 6'
+    'Project 2', 'Project 3', 'Project 4', 'Project 5', 'Project 6',
   ],
   tech:
   [
@@ -13,20 +13,49 @@ const worksInfo = {
   ],
   description:
   [
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    "Lorem Ipsuz is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea"
+    "Project1 text Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    "Project2 text Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    "Project3 text Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    "Project4 text Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    "Project5 text Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    "Project6 text Ipsuz is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
   ],
   projectImgMobile:
   [
-    'assets/images/p1Mobile.png'
+    'assets/images/p1Mobile.png',
+    'assets/images/p2Mobile.png',
+    'assets/images/p1Mobile.png',
+    'assets/images/p2Mobile.png',
+    'assets/images/p1Mobile.png',
+    'assets/images/p2Mobile.png',
   ],
   projectImgDesktop:
   [
+    'assets/images/p1Desktop.png',
+    'assets/images/p2Desktop.png',
+    'assets/images/p1Desktop.png',
+    'assets/images/p2Desktop.png',
+    'assets/images/p1Desktop.png',
+    'assets/images/p2Desktop.png',
 
+  ],
+  linkLive:
+  [
+    'https://gandradep.github.io/setupPortfolioMobileFirst/',
+    '#',
+    'https://gandradep.github.io/setupPortfolioMobileFirst/',
+    'https://gandradep.github.io/setupPortfolioMobileFirst/',
+    'https://gandradep.github.io/setupPortfolioMobileFirst/',
+    'https://gandradep.github.io/setupPortfolioMobileFirst/',
+  ],
+  linkSource:
+  [
+    'https://github.com/gandradep/setupPortfolioMobileFirst',
+    '#',
+    'https://github.com/gandradep/setupPortfolioMobileFirst',
+    'https://github.com/gandradep/setupPortfolioMobileFirst',
+    'https://github.com/gandradep/setupPortfolioMobileFirst',
+    'https://github.com/gandradep/setupPortfolioMobileFirst',
   ],
 };
 
@@ -57,13 +86,10 @@ for (let i = 0; i < worksInfo.title.length; i += 1) {
   }
 
   const button = document.createElement('a');
-  // const buttonText = document.createTextNode('See Project');
-  // button.appendChild(buttonText);
   button.innerText = 'See Project';
   button.className = 'button';
   button.href = '#';
   hgroup.appendChild(title);
-
   divCard.appendChild(hgroup);
   divCard.appendChild(techList);
   divCard.appendChild(button);
@@ -72,38 +98,78 @@ for (let i = 0; i < worksInfo.title.length; i += 1) {
 }
 
 /* create modal window */
-
 const modal = document.getElementById('modal');
 const buttons = Array.from(document.querySelectorAll('.workPost .button'));
 const cardDiv = document.createElement('div');
 cardDiv.classList.add('cardDiv');
 const cardImgMob = document.createElement('img');
-cardImgMob.classList.add('cardImgMob');
+cardImgMob.classList.add('width90', 'displayMobile');
+const cardImgDesk = document.createElement('img');
+cardImgDesk.classList.add('cardImgDesk', 'displayDesktop');
 const closeModal = document.createElement('i');
 closeModal.classList.add('fa-duotone', 'fa-x', 'closeModal');
-const modalTitle = document.createElement('h2');
-modalTitle.classList.add('modalTitle', 'darkColor');
+const modalTitleMob = document.createElement('h2');
+modalTitleMob.classList.add('modalTitle', 'darkColor', 'displayMobile');
+const modalTitleDesk = document.createElement('h2');
+modalTitleDesk.classList.add('modalTitle', 'darkColor', 'displayDesktop');
 const modalTechList = document.createElement('ul');
-modalTechList.classList.add('postColor');
-
-
+modalTechList.classList.add('postColor', 'width90');
+const projectDescription = document.createElement('p');
+projectDescription.classList.add('projectCardDescription', 'width90');
+const divButtons = document.createElement('div');
+divButtons.classList.add('divButtons', 'displayMobile');
+const buttonLive = document.createElement('a');
+buttonLive.classList.add('button', 'block', 'alignButton');
+buttonLive.target = '_blank';
+const liveText = document.createElement('span');
+liveText.innerText = 'See Live  ';
+const buttonSource = document.createElement('a');
+buttonSource.classList.add('button', 'block', 'alignButton');
+buttonSource.target = '_blank';
+const sourceText = document.createElement('span');
+sourceText.innerText = 'See Source ';
+const liveImg = document.createElement('img');
+liveImg.src = 'assets/images/liveIcon.png';
+const sourceImg = document.createElement('img');
+sourceImg.src = 'assets/images/sourceIcon.png';
+const divTitleAndButton = document.createElement('div');
+divTitleAndButton.classList.add('displayDesktop', 'divTextButton');
+const divButtonsDesktop = document.createElement('div');
+divButtonsDesktop.classList.add('divButtons');
+buttonLive.append(liveText, liveImg);
+buttonSource.append(sourceText, sourceImg);
+divButtons.append(buttonLive, buttonSource);
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    let index = buttons.indexOf(button);
+    const index = buttons.indexOf(button);
+    let numTech = 3;
     modal.classList.remove('displayNone');
     modal.classList.add('modalDisplay');
     cardImgMob.src = worksInfo.projectImgMobile[index];
-    modalTitle.innerText = worksInfo.title[index];
-    for (let j = 0; j <worksInfo.tech[index].length; j += 1) {
+    cardImgDesk.src = worksInfo.projectImgDesktop[index];
+    modalTitleMob.innerText = worksInfo.title[index];
+    modalTitleDesk.innerText = worksInfo.title[index];
+    projectDescription.innerText = worksInfo.description[index];
+    buttonLive.href = worksInfo.linkLive[index];
+    buttonSource.href = worksInfo.linkSource[index];
+    divButtonsDesktop.append(buttonLive.cloneNode(true), buttonSource.cloneNode(true));
+
+    /* only displays 3 use technologies for mobile screen */
+
+    numTech = worksInfo.tech[index].length;
+
+    for (let j = 0; j < numTech; j += 1) {
       const li = document.createElement('li');
       li.innerText = worksInfo.tech[index][j];
+      li.classList.add('singleTechCard');
       modalTechList.appendChild(li);
     }
-    cardDiv.appendChild(closeModal);
-    cardDiv.appendChild(cardImgMob);
-    cardDiv.appendChild(modalTitle);
-    cardDiv.appendChild(modalTechList);
+    divTitleAndButton.append(modalTitleDesk, divButtonsDesktop);
+    cardDiv.append(
+      closeModal, cardImgDesk, cardImgMob, divTitleAndButton, modalTitleMob,
+      modalTechList, projectDescription, divButtons,
+    );
     modal.appendChild(cardDiv);
   });
 });
@@ -111,7 +177,7 @@ buttons.forEach((button) => {
 closeModal.addEventListener('click', () => {
   modal.classList.remove('modalDisplay');
   modal.classList.add('displayNone');
-  modalTechList.innerText='';
-  modal.innerText='';
-
+  modalTechList.innerText = '';
+  modal.innerText = '';
+  divButtonsDesktop.innerText = '';
 });
