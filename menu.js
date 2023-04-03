e00f41cbfdc01ddda3f727092d4de4732b9e01f8
Gabriel Andrade <eventsonido@gmail.com>
2023-04-03 16:27:06 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,17 +1,14 @@
 const hamburgerSymbol = document.getElementById('hamburgerSymbol');
 const popUpMenuMobile = document.getElementById('popUpMenuMobile');
 const closeMenuSymbol = document.getElementById('closeMenuSymbol');
-const body = document.querySelector('body');
 const menuListItems = Array.from(document.querySelectorAll('ul .navBarColor'));
 
 function openMenu() {
-  popUpMenuMobile.classList.add('showMenu');
-  body.style.overflow = 'hidden';
+  popUpMenuMobile.classList.add('showMenu');  
 }
 
 function closeMenu() {
-  popUpMenuMobile.classList.remove('showMenu');
-  body.style.overflow = 'auto';
+  popUpMenuMobile.classList.remove('showMenu');  
 }
 
 hamburgerSymbol.addEventListener('click', openMenu);
